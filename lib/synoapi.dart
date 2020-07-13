f3119bfb0b8533e54d56c170807c8f81bdf61e29
Tony Ting <k999hoshing@gmail.com>
2020-07-13 12:47:55 +0800

    Clean up

--- lib/synoapi.dart
+++ lib/synoapi.dart
@@ -3,6 +3,13 @@
 /// More dartdocs go here.
 library synoapi;
 
-export 'src/synoapi_base.dart';
+export 'src/auth.dart';
+export 'src/const.dart';
+export 'src/context.dart';
+export 'src/downloadstation.dart';
+export 'src/query.dart';
+export 'src/modeled/downloadstation.dart';
+export 'src/modeled/model.dart';
+export 'src/modeled/query_mapped.dart';
 
 // TODO: Export any libraries intended for clients of this package.
